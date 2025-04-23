cc913239f564eb3276352596654c0d749cbc9888
Majid Darvishan <majid.darvishan@peykasa.ir>
2025-04-23 11:50:23 +0330

    Add https

--- proxy_service/main.go
+++ proxy_service/main.go
@@ -4,6 +4,12 @@ import (
 	"auth_service/config"
 	"auth_service/database"
 	"auth_service/routes"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 // @title Auth service API
@@ -31,5 +37,28 @@ func main() {
 	r := routes.SetupRoutes()
 
 	// Run the server on port 8080.
-	r.Run(":8080")
+	// r.Run(":8080")
+
+    // To ensure all HTTP requests are redirected to HTTPS, run a separate HTTP server.
+    go func() {
+        // HTTP server: Listen on port 8080 and redirect all traffic to HTTPS on port 8443.
+        httpRouter := gin.Default()
+        httpRouter.GET("/", func(c *gin.Context) {
+            // Clean host if it includes the HTTP port.
+            host := c.Request.Host
+            // Remove port if present
+            if colonPos := strings.Index(host, ":"); colonPos != -1 {
+                host, _, _ = net.SplitHostPort(host)
+            }
+            c.Redirect(http.StatusMovedPermanently, "https://" + host + ":8443" + c.Request.RequestURI)
+        })
+        if err := httpRouter.Run(":8080"); err != nil {
+            log.Fatal("HTTP redirection server failed:", err)
+        }
+    }()
+
+    // Run HTTPS server with SSL certificate on port 8443.
+    if err := r.RunTLS(":8443", "cert.pem", "key.pem"); err != nil {
+        log.Fatal("Failed to start HTTPS server:", err)
+    }
 }
